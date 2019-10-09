96c1668a539b6a2009d6588857a04f4c80443708
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-10-09 13:38:47 +0100

    Messed around with mapbox. Created a map for storytelling

--- scrollytelling/config.js
+++ scrollytelling/config.js
@@ -1,29 +1,62 @@
 var config = {
-    style: 'YOUR_STYLE_URL', //TODO
-    accessToken: 'YOUR_ACCESS_TOKEN', //TODO
-    title: 'YOUR MAP TITLE', //TODO
-    subtitle: 'YOUR MAP SUBTITLE', //TODO
-    byline: 'By YOUR NAME', //TODO
-    footer: 'Created at Concatenate Lagos 2019',
-    showMarkers: false,
-    theme: 'light',
-    alignment: 'left',
-    chapters: [
-        //TODO replace this data with data from the first point in your dataset
-        {
-            id: 'concatenate-lagos',
-            title: 'Concatenate Lagos',
-            description: 'A free conference for African developers with speakers from around the world.',
-            location: {
-              "center": [
-                3.366639462826271,
-                6.51635441424402
-              ],
-              "zoom": 11.805394255186213,
-              "pitch": 33.499999999999986,
-              "bearing": 136.79999999999995
-            }
-        },
-        //TODO Copy-paste more chapters here - one for each point in your dataset!
-    ]
+	style: 'mapbox://styles/chuck-huey/ck1j5acuq11481cmj9v3kxfnv', //TODO
+	accessToken:
+		'pk.eyJ1IjoiY2h1Y2staHVleSIsImEiOiJjazFqMjducXgwNjhqM25tdTE2MmxiY3ozIn0.R8Y-O3ZC1YIkE6KPTBdkAA', //TODO
+	title: 'A Short View of my life in Lagos', //TODO
+	subtitle: 'My Journey in Lagos, so far...', //TODO
+	byline: 'By Ochuko Ekrresa', //TODO
+	footer: "Created at Concatenate Lagos 2019, Anjana Vakil's Workshop on MapBox",
+	showMarkers: true,
+	theme: 'light',
+	alignment: 'left',
+	chapters: [
+		//TODO replace this data with data from the first point in your dataset
+		{
+			id: 'okota-lagos',
+			title: 'Okota, Lagos',
+			description: 'The first place I stayed in Lagos. The rent emptied my account at the time.',
+			location: {
+				center: [3.314588, 6.520303],
+				zoom: 16.62894241527768,
+				pitch: 60,
+				bearing: 12.7999999999995,
+			},
+		},
+		{
+			id: 'ihs-towers',
+			title: 'IHS Nigeria',
+			description:
+				'My first job in Lagos. Worked as a Field Engineer. I started working here during my youth service.',
+			location: {
+				center: [3.411829, 6.428367],
+				zoom: 16.62894241527768,
+				pitch: 60,
+				bearing: 12.7999999999995,
+			},
+		},
+		{
+			id: 'decagon',
+			title: 'Decagon Institute',
+			description:
+				'My first job as a Software Engineer. I was trained in web development, and the management refused to let me go, lol. Also, met my girlfriend here.',
+			location: {
+				center: [3.525621, 6.438452],
+				zoom: 16.62894241527768,
+				pitch: 60,
+				bearing: 12.7999999999995,
+			},
+		},
+		{
+			id: 'atlantic',
+			title: 'Atlantic View Estate',
+			description: 'My current home.',
+			location: {
+				center: [3.524019, 6.426156],
+				zoom: 16.62894241527768,
+				pitch: 60,
+				bearing: 12.7999999999995,
+			},
+		},
+		//TODO Copy-paste more chapters here - one for each point in your dataset!
+	],
 };
